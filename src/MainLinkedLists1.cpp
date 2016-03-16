c9ac1d7f31828876307be987ecbe7f2f6bafcbca
shivaprasad95 <shivaprasad95@missionrnd.com>
2016-03-16 13:41:48 +0530

    Submit Commit

--- src/MainLinkedLists1.cpp
+++ src/MainLinkedLists1.cpp
@@ -24,7 +24,40 @@ Objectives of C-LinkedLists-1 Lesson:
 
 //Do not edit below Header Files
 #include <stdio.h>
+#include <malloc.h>
+#include "FunctionHeadersLinkedLists1.h"
+struct node {
+	int num;
+	struct node *next;
+};
+struct node * createNode(int num) {
+	struct node *newNode = (struct node *)malloc(sizeof(struct node));
+	newNode->num = num;
+	newNode->next = NULL;
+	return newNode;
+}
 
+struct node * createList(int num) {
+	struct node *head = createNode(num % 10);
+
+	num /= 10;
+	while (num) {
+		struct node *newNode = createNode(num % 10);
+	
+		newNode->next = head;
+		head = newNode;
+		num /= 10;
+	}
+	return head;
+}
+bool compare(struct node *result, int num) {
+	struct node *head = createList(num);
+	while (head && result && head->num == result->num) {
+		head = head->next;
+		result = result->next;
+	}
+	return !head && !result;
+}
 
 int main(){
 
@@ -33,7 +66,13 @@ int main(){
 	//Test numberToLinkedList
 	
 	//Test removeEveryKthNode
-	
+	struct node * temp1 = sortLinkedList(createList(9876543210));
+
+	while (temp1 != NULL)
+	{
+		printf("\n%d  |", temp1->num);
+		temp1 = temp1->next;
+	}
 	//Test sortLinkedList
 
 	//Test twodigitLinkedList
--- src/sortLinkedList.cpp
+++ src/sortLinkedList.cpp
@@ -19,5 +19,43 @@ struct node {
 };
 
 struct node * sortLinkedList(struct node *head) {
-	return NULL;
+
+	struct node * list = head;
+	int l = 0;
+	int array[10];
+
+	if (head == NULL)
+		return NULL;
+
+	//finding length of the linked list
+	while (list != NULL)
+	{
+		array[l++] = list->num;
+		list = list->next;
+	}
+
+	int i, j, temp;
+	struct node *temp1, *temp2; //no extra linked list is used ..temp1 & temp2 referencing same head
+
+	temp1 = head;
+	for (i = 0; i < l - 1; i++)
+	{
+		temp2 = temp1->next;
+
+		for (j = i + 1; j < l; j++)
+		{
+			if (temp1->num > temp2->num)
+			{
+				//swapping
+				temp = temp1->num;
+				temp1->num = temp2->num;
+				temp2->num = temp;
+			}
+			temp2 = temp2->next;
+		}
+		temp1 = temp1->next;
+	}
+
+	
+	return head;
 }
